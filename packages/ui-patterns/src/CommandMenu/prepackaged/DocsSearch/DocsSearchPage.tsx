57ad780736429325905a2ea445975001ed8dc96b
Charis Lam <26616127+charislam@users.noreply.github.com>
2026-02-13 14:42:58 -0500

    fix(docs): add troubleshooting type to search ui

--- packages/ui-patterns/src/CommandMenu/prepackaged/DocsSearch/DocsSearchPage.tsx
+++ packages/ui-patterns/src/CommandMenu/prepackaged/DocsSearch/DocsSearchPage.tsx
@@ -1,16 +1,14 @@
 'use client'
 
-import { Book, ChevronRight, Github, Hash, Loader2, MessageSquare, Search } from 'lucide-react'
-import { useEffect, useRef } from 'react'
-
 import {
-  DocsSearchResultType as PageType,
-  useDocsSearch,
   type DocsSearchResult as Page,
   type DocsSearchResultSection as PageSection,
+  DocsSearchResultType as PageType,
+  useDocsSearch,
 } from 'common'
+import { Book, ChevronRight, Github, Hash, Loader2, MessageSquare, Search } from 'lucide-react'
+import { useEffect, useRef } from 'react'
 import { Button, CommandGroup_Shadcn_, CommandItem_Shadcn_, CommandList_Shadcn_, cn } from 'ui'
-import { StatusIcon } from 'ui/src/components/StatusIcon'
 
 import {
   Breadcrumb,
@@ -26,6 +24,7 @@ import {
   useSetQuery,
 } from '../..'
 import { BASE_PATH } from '../shared/constants'
+import { StatusIcon } from 'ui/src/components/StatusIcon'
 
 const questions = [
   'How do I get started with Supabase?',
@@ -300,8 +299,9 @@ export function formatSectionUrl(page: Page, section: PageSection) {
       return `${page.path}#${section.slug ?? ''}`
     case PageType.Reference:
       return `${page.path}/${section.slug ?? ''}`
+    case PageType.Troubleshooting:
+    // [Charis] Markdown headings on integrations pages don't have slugs yet
     case PageType.Integration:
-      // [Charis] Markdown headings on integrations pages don't have slugs yet
       return page.path
     default:
       throw new Error(`Unknown page type '${page.type}'`)
@@ -313,6 +313,7 @@ export function getPageIcon(page: Page) {
     case PageType.Markdown:
     case PageType.Reference:
     case PageType.Integration:
+    case PageType.Troubleshooting:
       return <Book strokeWidth={1.5} className="!mr-0 !w-4 !h-4" />
     case PageType.GithubDiscussion:
       return <Github strokeWidth={1.5} className="!mr-0 !w-4 !h-4" />
@@ -326,6 +327,7 @@ export function getPageSectionIcon(page: Page) {
     case PageType.Markdown:
     case PageType.Reference:
     case PageType.Integration:
+    case PageType.Troubleshooting:
       return <Hash strokeWidth={1.5} className="!mr-0 !w-4 !h-4" />
     case PageType.GithubDiscussion:
       return <MessageSquare strokeWidth={1.5} className="!mr-0 !w-4 !h-4" />
